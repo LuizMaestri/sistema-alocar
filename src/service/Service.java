76ee2b5683b3cea049635c65919882ef3afe990f
LuizMaestri <luizricardomaestri@gmail.com>
2016-06-09 01:06:46 -0300

    add abstract service class

---
+++ src/service/Service.java
@@ -0,0 +1,24 @@
+package service;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author luiz
+ * @version 1
+ * @since 09/06/16
+ */
+public abstract class Service<T, N> {
+    protected HashMap<N, Integer> indexes;
+    protected List<T> list;
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public abstract void fetch();
+    public abstract T get(N id);
+    public abstract boolean save(T entity);
+    public abstract boolean saveAll(List<T> entities);
+    public abstract void delete(N id);
+}
