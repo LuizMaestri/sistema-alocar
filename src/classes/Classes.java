309df9ea617cba33ead8185379a22449e782d4b7
LuizMaestri <luizricardomaestri@gmail.com>
2016-06-10 00:12:35 -0300

    change _id attribute from long primitive to Long class

---
+++ src/classes/Classes.java
@@ -0,0 +1,9 @@
+package classes;
+
+/**
+ * @author luiz
+ * @version 1
+ * @since 09/06/16
+ */
+public class Classes {
+}
---
+++ src/gpda/GPDA.java
@@ -0,0 +1,9 @@
+package gpda;
+
+/**
+ * @author luiz
+ * @version 1
+ * @since 10/06/16
+ */
+public class GPDA {
+}
