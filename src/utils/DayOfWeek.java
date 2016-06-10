29464286c120c239d499c8c0dcc6a5d47913cdbb
LuizMaestri <luizricardomaestri@gmail.com>
2016-06-09 21:08:48 -0300

    create enum to ddays of week

---
+++ src/utils/DayOfWeek.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * @author luiz
+ * @version 1
+ * @since 09/06/16
+ */
+public enum DayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+}
