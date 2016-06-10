dfe481d7faa6afd136c54e57fee1f2b8ed07b698
LuizMaestri <luizricardomaestri@gmail.com>
2016-06-10 00:00:12 -0300

    add to project's constants minimum credits and maximum workload to professor

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -10,4 +10,6 @@ public class Constants {
     public static final String DB_NAME = "ALOCAR";
     public static final String MONGO_HOST = "localhost";
     public static final int MONGO_PORT = 27017;
+    public static final int CREDITS_MIN = 8;
+    public static final int MAXIMUM_WORKLOAD = 40;
 }
