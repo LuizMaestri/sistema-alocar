40c079b1526c87e396b2b743a03d579c55a7b2c5
LuizMaestri <luizricardomaestri@gmail.com>
2016-06-15 23:45:35 -0300

    for invalid Id

---
+++ src/exception/IdException.java
@@ -0,0 +1,13 @@
+package exception;
+
+/**
+ * @author luiz
+ * @version 1
+ * @since 15/06/16
+ */
+public class IdException extends Exception{
+
+    public IdException(String message) {
+        super(message);
+    }
+}
