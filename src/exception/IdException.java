dcad4ca8ae168eae7e0a51ae6b38f5ddcf631dce
LuizMaestri <luizricardomaestri@gmail.com>
2016-06-16 19:20:22 -0300

    recreate id exception

--- src/exception/IdException.java
+++ src/exception/IdException.java
@@ -3,9 +3,9 @@ package exception;
 /**
  * @author luiz
  * @version 1
- * @since 15/06/16
+ * @since 16/06/16
  */
-public class IdException extends Exception{
+public class IdException extends Exception {
 
     public IdException(String message) {
         super(message);
