f544cf9e12dbf70b881d13844bb94b5b1cd60698
LuizMaestri <luizricardomaestri@gmail.com>
2016-07-02 20:13:25 -0300

    list professor and gpda's professor to association

---
+++ src/controller/AssociationController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import gpda.GPDAService;
+import professor.Professor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static utils.Constants.CONTROLLER;
+
+/**
+ * Created by loja on 02/07/2016.
+ */
+public class AssociationController implements IController{
+
+    private GPDAService gpdaService;
+
+    public AssociationController() {
+        loadData();
+    }
+
+    @Override
+    public void loadData() {
+        gpdaService = new GPDAService();
+    }
+
+    public List<Professor> getGpdaProfessors(){
+       return gpdaService.getByCoordinator(CONTROLLER.getLoggedUser()).getProfessors();
+    }
+
+    public ArrayList<Professor> getProfessor(){
+        ArrayList<Professor> professors = CONTROLLER.getUserService().getList();
+        professors.removeAll(getGpdaProfessors());
+        return professors;
+    }
+}
