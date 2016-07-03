6b1500934d973682f85b0cf8c28fffc4ae28b489
Fabio D. R <delaroca.fabio@gmail.com>
2016-07-03 08:05:40 -0300

    mensagem de id inválido

--- src/utils/ErrorMessage.java
+++ src/utils/ErrorMessage.java
@@ -8,5 +8,6 @@ package utils;
 public class ErrorMessage {
     public static final String ALLOCATE_PROFESSOR = "A turma %c da disciplina %s ficou sem professor.";
     public static final String ALLOCATE_ROOM = "N�o existe sala para %d alunos livres nos hor�rios das turma %c da disciplina %s.";
-    public static final String INVALID_lOGIN = "Usuário e/ou senha inválidos";
+    public static final String INVALID_lOGIN = "Usuário e/ou senha inválidos.";
+    public static final String INVALID_ID = "Ocorreu um erro no banco de dados, tente novamente mais tarde.";
 }
