eefc5619ba5b5d46cfa3a344b380e3771ba46cf6
LuizMaestri <luizricardomaestri@gmail.com>
2016-07-06 23:25:18 -0300

    hour strings now in Constants

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -15,4 +15,9 @@ public class Constants {
     public static final int CREDITS_MIN = 8;
     public static final int MAXIMUM_WORKLOAD = 40;
     public static final UserController CONTROLLER = new UserController();
+    public static final String[] HOURS = {
+            "07:30 - 08:20", "08:20 - 09:10", "09:10 - 10:00", "10:10 - 11:00", "11:00 - 11:50",
+            "13:30 - 14:20", "14:20 - 15:10", "15:10 - 16:00", "16:20 - 17:10", "17:10 - 18:00",
+            "18:30 - 19:20", "19:20 - 20:10", "20:20 - 21:10", "21:10 - 22:00"
+    };
 }
