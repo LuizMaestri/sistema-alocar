cd27e1acee9e9ee28a11bbda2cd7c5b5510573cd
LuizMaestri <luizricardomaestri@gmail.com>
2016-07-09 03:21:02 -0300

    not print yet

--- src/view/panels/AlocarTurma.java
+++ src/view/panels/AlocarTurma.java
@@ -1,38 +1,68 @@
 package view.panels;
 
+import allocate.Allocate;
+import controller.AllocationController;
+import view.manager.UIManager;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
+
+import static utils.Constants.HOURS;
 
 public class AlocarTurma extends JPanel {
 	private static final long serialVersionUID = 1L;
-	private JTable table;
 
 	public AlocarTurma() {
 		setLayout(null);
 
-		JButton btnImprimir = new JButton("Imprimir");
-		btnImprimir.setBounds(300, 486, 89, 27);
-		add(btnImprimir);
+        AllocationController controller = new AllocationController();
 
 		JButton btnAlocar = new JButton("Alocar");
-		btnAlocar.setBounds(446, 488, 89, 27);
-		add(btnAlocar);
+		btnAlocar.addActionListener(e -> {
 
-		JButton btnSair = new JButton("Sair");
-		btnSair.setBounds(603, 488, 89, 27);
-		add(btnSair);
+        });
+		add(btnAlocar).setBounds(446, 488, 89, 27);
 
-		String[] colunas = { "Professor", "Turma", "Disciplina", "Horário", "Créditos", "Sala" };
-		Object[][] dados = { { "Fulano", "3456", "APS", "18:30", "4", "109" },
-				{ "Ciclano", "3456", "APS", "18:30", "4", "109" } };
-		table = new JTable(dados, colunas);
+		JButton btnSair = new JButton("Voltar");
+		btnSair.addActionListener(e -> UIManager.setPanel(new Menu()));
+		add(btnSair).setBounds(603, 488, 89, 27);
+
+		Allocate allocate = controller.getInfos();
+		DefaultTableModel tableModel = new DefaultTableModel();
+		for (String coluna : new String[] { "Turma", "Professor", "Sala", "Horário", "Créditos" })
+			tableModel.addColumn(coluna);
+		if (allocate != null)
+			allocate.getRooms().forEach(room ->
+					room.getOccupation().entrySet().forEach(day ->
+							controller.getClasses(day.getValue()).forEach(classes -> {
+								ArrayList<String> hourString = new ArrayList<>();
+								classes.getHorary().get(day.getKey()).forEach(hour ->hourString.add(HOURS[hour]));
+								Object[] linha = new Object[5];
+								linha[0] = classes.toString();
+								linha[1] = classes.getProfessor().getName();
+								linha[2] = room.toString();
+								linha[3] = day + "  " +
+										hourString.get(0).split("-")[0] + "-" +
+										hourString.get(hourString.size() - 1).split("-")[1];
+								linha[0] = hourString.size();
+								tableModel.addRow(linha);
+							})
+					)
+			);
+		JTable table = new JTable(tableModel);
 		table.getTableHeader().setReorderingAllowed(false);
 
-		JScrollPane scroll = new JScrollPane(table);
-		scroll.setBounds(47, 60, 901, 355);
-		add(scroll);
+		add(new JScrollPane(table)).setBounds(47, 60, 901, 355);
+
+        JButton btnImprimir = new JButton("Imprimir");
+        btnImprimir.addActionListener(e -> {
+
+        });
+        add(btnImprimir).setBounds(300, 486, 89, 27);
 
 	}
 }
